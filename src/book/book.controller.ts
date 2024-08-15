7482afd497c2644eca75f04a8a06a821946bc929
ByronMike <auger.michaell@gmail.com>
2024-08-15 15:03:11 +0200

    feat: get by id route

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -12,6 +12,11 @@ export class BookController {
     return this.bookService.findAll();
   }
 
+  @Get(':id')
+  async getBook(@Param('id') id: string): Promise<Book> {
+    return this.bookService.findById(id);
+  }
+
   @Post()
   async createBook(@Body() book: CreateBookDto): Promise<Book> {
     return this.bookService.create(book);
